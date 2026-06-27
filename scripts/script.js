a6a1c61728afe67a997060001a2b7a8d0b3e0d5c
alexanderjneville <alexanderjneville90@gmail.com>
2026-06-27 15:04:48 -0600

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,8 @@
-/* CSS Reset */
-body, header, nav, main, footer, h1, h2, p, ul, li {
-    margin: 0;
-    padding: 0;
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
 }
 
-/* Body */
-body {
-    background-color: #f4f4f4;
-    font-family: Arial, sans-serif;
-}
-
-/* Header */
-header {
-    background-color: #4CAF50;
-    color: white;
-    text-align: center;
-    padding: 20px;
-}
-
-/* Navigation */
-nav {
-    background-color: #333;
-}
-
-nav ul {
-    list-style-type: none;
-}
-
-nav li {
-    padding: 15px;
-}
-
-nav a {
-    color: white;
-    text-decoration: none;
-}
-
-/* Main */
-main {
-    padding: 20px;
-}
-
-/* Footer */
-footer {
-    background-color: #4CAF50;
-    color: white;
-    text-align: center;
-    padding: 10px;
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 }
