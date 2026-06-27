760730ba4b1703c1593419130fda6d1d7786da6d
alexanderjneville <alexanderjneville90@gmail.com>
2026-06-27 16:45:03 -0600

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
   } else {
-    x.className = "topnav";
+    x.style.display = "block";
   }
 }
