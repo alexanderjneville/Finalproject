66e619587dc7876b02ce641416f6ce7a5dfe0814
alexanderjneville <alexanderjneville90@gmail.com>
2026-06-27 15:44:09 -0600

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
-
